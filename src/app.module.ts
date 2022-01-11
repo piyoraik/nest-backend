7e00e9a39af3698c082fe79500570fac3f9f0169
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-11 20:27:29 +0900

    enduser各種作った

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuctionModule } from './auction/auction.module';
+import { EndusersController } from './endusers/endusers.controller';
+import { EndusersService } from './endusers/endusers.service';
+import { EndusersModule } from './endusers/endusers.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuctionModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(), AuctionModule, EndusersModule],
+  controllers: [AppController, EndusersController],
+  providers: [AppService, EndusersService],
 })
 export class AppModule {}
