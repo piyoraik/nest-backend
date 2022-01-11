5e24eb36cb21246ff6dc38dd1d5488bc6224f5b9
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-11 17:40:40 +0900

    EndUserServiceにTypeORM用のコンストラクタを設定

--- src/endusers/endusers.service.ts
+++ src/endusers/endusers.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Admin } from 'src/entity/admin.entity';
+import { AfterSuccessfulBid } from 'src/entity/after.successful.bid.entity';
+import { Auction } from 'src/entity/auction.entity';
+import { AuctionListing } from 'src/entity/auction.listing.entity';
+import { Color } from 'src/entity/color.entity';
+import { DeliveryMethod } from 'src/entity/delivery.method.entity';
+import { Members } from 'src/entity/member.entity';
+import { Option } from 'src/entity/option.entity';
+import { PaymentMethods } from 'src/entity/payment.methods.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class EndusersService {}
+export class EndusersService {
+  constructor(
+    private readonly memberRepo: Repository<Members>,
+    private readonly actionRepo: Repository<Auction>,
+    private readonly actionListingRepo: Repository<AuctionListing>,
+    private readonly optionRepo: Repository<Option>,
+    private readonly deliveryRepo: Repository<DeliveryMethod>,
+    private readonly paymentRepo: Repository<PaymentMethods>,
+    private readonly purchaseRepo: Repository<Auction>,
+    private readonly afterSuccessRepo: Repository<AfterSuccessfulBid>,
+    private readonly colorRepo: Repository<Color>,
+    private readonly adminRepo: Repository<Admin>,
+  ) {}
+}
