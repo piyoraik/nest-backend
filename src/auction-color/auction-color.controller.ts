6bb986b464ba379a103879442ccd57a9d556460a
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-11 23:50:55 +0900

    color登録API作成

--- src/auction-color/auction-color.controller.ts
+++ src/auction-color/auction-color.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuctionColorService } from './auction-color.service';
+import { CreateAuctionColorDTO } from './dto/create-auctioncolor-dto';
 
 @Controller('auction-color')
 export class AuctionColorController {
@@ -8,5 +9,10 @@ export class AuctionColorController {
   @Get()
   fetchAll() {
     return this.auctionColorService.fetchAll();
+  }
+
+  @Post()
+  create(@Body() body: CreateAuctionColorDTO) {
+    return this.auctionColorService.create(body);
   }
 }
--- src/auction-color/auction-color.service.ts
+++ src/auction-color/auction-color.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuctionColor } from 'src/entity/auction.color.entity';
 import { Repository } from 'typeorm';
+import { CreateAuctionColorDTO } from './dto/create-auctioncolor-dto';
 
 @Injectable()
 export class AuctionColorService {
@@ -12,5 +13,10 @@ export class AuctionColorService {
 
   fetchAll() {
     return this.auctionColorRepo.find();
+  }
+
+  create(createAuctionColorDTO: CreateAuctionColorDTO) {
+    const color = this.auctionColorRepo.create(createAuctionColorDTO);
+    return this.auctionColorRepo.save(color);
   }
 }
