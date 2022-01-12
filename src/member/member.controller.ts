21436e4d05ccc7db60f21f65d9ed7028e72b193d
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-12 16:19:12 +0900

    whitelistをtrueに、memberのcreate作成

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateMemberDTO } from './dto/create-member-dto';
 import { MemberService } from './member.service';
 
 @Controller('member')
@@ -8,5 +9,10 @@ export class MemberController {
   @Get()
   fetchAll() {
     return this.memberService.findAll();
+  }
+
+  @Post()
+  create(@Body() body: CreateMemberDTO) {
+    return this.memberService.create(body);
   }
 }
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Members } from 'src/entity/member.entity';
 import { Repository } from 'typeorm';
+import { CreateMemberDTO } from './dto/create-member-dto';
 
 @Injectable()
 export class MemberService {
@@ -11,5 +12,10 @@ export class MemberService {
 
   findAll() {
     return this.memberRepo.find();
+  }
+
+  create(createMemberDTO: CreateMemberDTO) {
+    const member = this.memberRepo.create(createMemberDTO);
+    return this.memberRepo.save(member);
   }
 }
