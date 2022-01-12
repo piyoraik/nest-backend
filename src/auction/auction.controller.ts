729f4c41d6a99a329d40e749092f4751b6aa7bd5
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-12 18:38:24 +0900

    Auctionの1件取得・更新・論理削除実装

--- src/auction/auction.controller.ts
+++ src/auction/auction.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { AuctionService } from './auction.service';
 import { CreateAuctionDto } from './dto/create-auction.dto';
+import { UpdateAuctionDto } from './dto/update-auction.dto';
 
 @Controller('auction')
 export class AuctionController {
@@ -14,5 +23,20 @@ export class AuctionController {
   @Post()
   create(@Body() body: CreateAuctionDto) {
     return this.auctionService.create(body);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.auctionService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: UpdateAuctionDto) {
+    return this.auctionService.update(+id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.auctionService.softDelete(+id);
   }
 }
