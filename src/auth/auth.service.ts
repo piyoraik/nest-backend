4d7e2f7e546ac99e4fd8e4404641124fa5e98970
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-13 12:19:52 +0900

    認証用のメソッド作成

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { MemberService } from 'src/member/member.service';
 
 @Injectable()
@@ -6,6 +10,15 @@ export class AuthService {
   constructor(private memberService: MemberService) {}
 
   async validateUser(name: string, password: string) {
-    // const member = await this.memberService.findOne
+    const member = await this.memberService.findOne('name', name);
+    if (!member) {
+      throw new NotFoundException('Member Not Found');
+    }
+    if (member && member.password === password) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = member;
+      return result;
+    }
+    throw new UnauthorizedException();
   }
 }
