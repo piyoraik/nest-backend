75258a08809792d157448b62d0412d3d168770a8
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-15 23:18:20 +0900

    auctionColorのAPI削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuctionModule } from './auction/auction.module';
-import { AuctionColorModule } from './auction-color/auction-color.module';
 import { MemberModule } from './member/member.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(),
-    AuctionModule,
-    AuctionColorModule,
-    MemberModule,
-    AuthModule,
-  ],
+  imports: [TypeOrmModule.forRoot(), AuctionModule, MemberModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
