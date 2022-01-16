a04b76c1a6c5d1ccc7085bfff273067068b448de
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-16 21:50:03 +0900

    検査記録のサービス作成

--- src/testing-record/testing-record.service.ts
+++ src/testing-record/testing-record.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { ListingCar } from 'src/entity/listing.car.entity';
+import { CreateTestingRecordDTO } from './dto/create-testingrecord.dto';
 import { TestingRecordRepository } from './testing-record.repository';
 
 @Injectable()
@@ -6,4 +8,33 @@ export class TestingRecordService {
   constructor(
     private readonly testingRecordRepository: TestingRecordRepository,
   ) {}
+  async findAll() {
+    return await this.testingRecordRepository.find();
+  }
+
+  async create(
+    createTestingRecordDTO: CreateTestingRecordDTO,
+    listingCar: ListingCar,
+  ) {
+    return this.testingRecordRepository.createTestingRecord(
+      createTestingRecordDTO,
+      listingCar,
+    );
+  }
+
+  async findOne(id: number) {
+    return await this.testingRecordRepository.findOneTestingRecord({ id });
+  }
+
+  async find(attrs: Partial<ListingCar>) {
+    return await this.testingRecordRepository.findWhereTestingRecord(attrs);
+  }
+
+  async update(id: number, attrs: Partial<ListingCar>) {
+    return await this.testingRecordRepository.updateTestingRecord(id, attrs);
+  }
+
+  async delete(id: number) {
+    return await this.testingRecordRepository.softDeleteTestingRecord(id);
+  }
 }
