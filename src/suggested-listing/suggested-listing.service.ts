ae472307249154298ff7a78b8608f6aea0f024e0
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-17 14:06:25 +0900

    希望出品のサービス完成

--- src/suggested-listing/suggested-listing.service.ts
+++ src/suggested-listing/suggested-listing.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { ListingCar } from 'src/entity/listing.car.entity';
+import { SuggestedListing } from 'src/entity/suggested.listing.entity';
+import { CreateSuggestedListingDTO } from './dto/create.suggested-listing.dto';
+import { SuggestedListingRepository } from './suggested-listing.repository';
 
 @Injectable()
-export class SuggestedListingService {}
+export class SuggestedListingService {
+  constructor(
+    private readonly suggestedListingRepository: SuggestedListingRepository,
+  ) {}
+
+  async create(
+    createSuggestedListingDTO: CreateSuggestedListingDTO,
+    listingCar: ListingCar,
+  ) {
+    return await this.suggestedListingRepository.createSuggestedListing(
+      createSuggestedListingDTO,
+      listingCar,
+    );
+  }
+
+  async findOneId(id: number) {
+    return await this.suggestedListingRepository.findOneSuggestedListing({
+      id,
+    });
+  }
+
+  async findOne(attrs: Partial<SuggestedListing>) {
+    return await this.suggestedListingRepository.findOneSuggestedListing(attrs);
+  }
+
+  async find(attrs: Partial<SuggestedListing>) {
+    return await this.suggestedListingRepository.findOneSuggestedListing(attrs);
+  }
+
+  async delete(id: number) {
+    return await this.suggestedListingRepository.softDeleteSuggestedListing(id);
+  }
+}
