a7f8a230801612a3f77dddcd42408a40a99e82eb
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-21 18:55:49 +0900

    auctionListingのリポジトリ作成

--- src/auction-listing/auction-listing.module.ts
+++ src/auction-listing/auction-listing.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MembersModule } from 'src/members/members.module';
 import { AuctionListingController } from './auction-listing.controller';
 import { AuctionListingRepository } from './auction-listing.repository';
 import { AuctionListingService } from './auction-listing.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuctionListingRepository])],
+  imports: [
+    TypeOrmModule.forFeature([AuctionListingRepository]),
+    MembersModule,
+  ],
   controllers: [AuctionListingController],
   providers: [AuctionListingService],
 })
--- src/auction-listing/auction-listing.service.ts
+++ src/auction-listing/auction-listing.service.ts
@@ -1,7 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { create } from 'domain';
+import { Auction } from 'src/entity/auction.entity';
+import { CarBodyNumber } from 'src/entity/car.body.number.entity';
 import { AuctionListingRepository } from './auction-listing.repository';
+import { CreateAuctionListingDTO } from './dto/create.auction-listing.dto';
+import { PayLoad } from 'src/auth/interfaces/payload-interfaces';
+import { MembersService } from 'src/members/members.service';
+import { AuctionListing } from 'src/entity/auction.listing.entity';
 
 @Injectable()
 export class AuctionListingService {
-  constructor(private auctionListingRepository: AuctionListingRepository) {}
+  constructor(
+    private auctionListingRepository: AuctionListingRepository,
+    private membersService: MembersService,
+  ) {}
+  async create(
+    createAuctionListingDTO: CreateAuctionListingDTO,
+    payLoad: PayLoad,
+    auction: Auction,
+    carBodyNumber: CarBodyNumber,
+  ) {
+    const member = await this.membersService.findOne({ email: payLoad.email });
+    return this.auctionListingRepository.createAuctionListing(
+      createAuctionListingDTO,
+      member,
+      auction,
+      carBodyNumber,
+    );
+  }
+
+  async findOneID(id: number) {
+    return await this.auctionListingRepository.findOneAuctionListing({ id });
+  }
+
+  async findOne(attrs: Partial<AuctionListing>) {
+    return await this.auctionListingRepository.findOneAuctionListing(attrs);
+  }
+
+  // update
+  async update(id: number, attrs: Partial<AuctionListing>) {
+    return await this.auctionListingRepository.updateAuctionListing(id, attrs);
+  }
+
+  // softDelete
+  async softDelete(id: number) {
+    return await this.auctionListingRepository.softDeleteAuctionListing(id);
+  }
 }
