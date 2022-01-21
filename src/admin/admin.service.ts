d691f3d58ccf95518f94a07c9763230e43b4dfb9
ryusei-ota <ota.ryusei@gmail.com>
2022-01-22 02:36:59 +0900

    adminerr

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,14 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Admin } from 'typeorm';
 import { AdminRepository } from './admin.repository';
 import { CreateAdminDTO } from './dto/create.admin.dto';
 
 @Injectable()
+// admin.service.ts
 export class AdminService {
-  AdminRepository: any;
   constructor(private adminRepository: AdminRepository) {}
 
   // create
   async create(createAdminDTO: CreateAdminDTO) {
-    return this.AdminRepository.createAdmin(createAdminDTO);
+    return await this.adminRepository.createAdmin(createAdminDTO);
+  }
+
+  // findAll
+  async findAll() {
+    return await this.adminRepository.find();
+  }
+
+  // findOneID
+  async findOneID(id: number) {
+    return await this.adminRepository.findOneAdmin({ id });
+  }
+
+  // findOne
+  async findOne(attrs: Partial<Admin>) {
+    return await this.adminRepository.findOneAdmin(attrs);
+  }
+
+  // findWhere
+  async findWhere(attrs: Partial<Admin>) {
+    return await this.adminRepository.findWhereLikeAdmin(attrs);
+  }
+
+  // update
+  async update(id: number, attrs: Partial<Admin>) {
+    return await this.adminRepository.updateAdmin(id, attrs);
+  }
+
+  // softDelete
+  async softDelete(id: number) {
+    return await this.adminRepository.softDeleteAdmin(id);
   }
 }
