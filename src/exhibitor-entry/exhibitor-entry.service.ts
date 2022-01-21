82b48985169f3a881d3fd7db4cdbf0b80b6dfff5
ryusei-ota <ota.ryusei@gmail.com>
2022-01-22 01:46:17 +0900

    listing_carまわりのservice終了

--- src/exhibitor-entry/exhibitor-entry.service.ts
+++ src/exhibitor-entry/exhibitor-entry.service.ts
@@ -1,19 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { ExhibitorEntry } from 'src/entity/exhibitor.entry.entity';
 import { ListingCar } from 'src/entity/listing.car.entity';
 import { CreateExhibitorEntryDTO } from './dto/create.exhibitor-entry.dto';
 import { ExhibitorEntryRepository } from './exhibitor-entry.repository';
 
 @Injectable()
+
+// exhibitorEntry.service.ts
 export class ExhibitorEntryService {
   constructor(private exhibitorEntryRepository: ExhibitorEntryRepository) {}
 
+  // create
   async create(
     createExhibitorEntryDTO: CreateExhibitorEntryDTO,
     listingCar: ListingCar,
   ) {
-    return this.exhibitorEntryRepository.createExhibitorEntry(
+    return await this.exhibitorEntryRepository.createExhibitorEntry(
       createExhibitorEntryDTO,
       listingCar,
     );
+  }
+
+  // findAll
+  async findAll() {
+    return await this.exhibitorEntryRepository.find();
+  }
+
+  // findOneID
+  async findOneID(id: number) {
+    return await this.exhibitorEntryRepository.findOneExhibitorEntry({ id });
+  }
+
+  // findOne
+  async findOne(attrs: Partial<ExhibitorEntry>) {
+    return await this.exhibitorEntryRepository.findOneExhibitorEntry(attrs);
+  }
+
+  // findWhere
+  async findWhere(attrs: Partial<ExhibitorEntry>) {
+    return await this.exhibitorEntryRepository.findWhereLikeExhibitorEntry(
+      attrs,
+    );
+  }
+
+  // update
+  async update(id: number, attrs: Partial<ExhibitorEntry>) {
+    return await this.exhibitorEntryRepository.updateExhibitorEntry(id, attrs);
+  }
+
+  // softDelete
+  async softDelete(id: number) {
+    return await this.exhibitorEntryRepository.softDeleteExhibitorEntry(id);
   }
 }
--- src/suggested-listing/suggested-listing.service.ts
+++ src/suggested-listing/suggested-listing.service.ts
@@ -5,11 +5,11 @@ import { CreateSuggestedListingDTO } from './dto/create.suggested-listing.dto';
 import { SuggestedListingRepository } from './suggested-listing.repository';
 
 @Injectable()
+// suggestedListing.service.ts
 export class SuggestedListingService {
-  constructor(
-    private readonly suggestedListingRepository: SuggestedListingRepository,
-  ) {}
+  constructor(private suggestedListingRepository: SuggestedListingRepository) {}
 
+  // create
   async create(
     createSuggestedListingDTO: CreateSuggestedListingDTO,
     listingCar: ListingCar,
@@ -20,21 +20,40 @@ export class SuggestedListingService {
     );
   }
 
-  async findOneId(id: number) {
+  // findAll
+  async findAll() {
+    return await this.suggestedListingRepository.find();
+  }
+
+  // findOneID
+  async findOneID(id: number) {
     return await this.suggestedListingRepository.findOneSuggestedListing({
       id,
     });
   }
 
+  // findOne
   async findOne(attrs: Partial<SuggestedListing>) {
     return await this.suggestedListingRepository.findOneSuggestedListing(attrs);
   }
 
-  async find(attrs: Partial<SuggestedListing>) {
-    return await this.suggestedListingRepository.findOneSuggestedListing(attrs);
+  // findWhere
+  async findWhere(attrs: Partial<SuggestedListing>) {
+    return await this.suggestedListingRepository.findWhereSuggestedListing(
+      attrs,
+    );
   }
 
-  async delete(id: number) {
+  // update
+  async update(id: number, attrs: Partial<SuggestedListing>) {
+    return await this.suggestedListingRepository.updateSuggestedListing(
+      id,
+      attrs,
+    );
+  }
+
+  // softDelete
+  async softDelete(id: number) {
     return await this.suggestedListingRepository.softDeleteSuggestedListing(id);
   }
 }
