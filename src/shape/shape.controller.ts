17b63a71e2a0bc1301b66cf07508ae702f39e6cb
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-22 12:06:15 +0900

    Shape登録・参照できた

--- src/shape/shape.controller.ts
+++ src/shape/shape.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateShapeDTO } from './dto/create.shape.dto';
 import { ShapeService } from './shape.service';
 
@@ -6,13 +6,18 @@ import { ShapeService } from './shape.service';
 export class ShapeController {
   constructor(private readonly shapeService: ShapeService) {}
 
-  // @Get()
-  // findAll() {
-  //   return this.shapeService.findAll();
-  // }
+  @Get()
+  findAll() {
+    return this.shapeService.findAll();
+  }
 
-  // @Post()
-  // create(@Body() body: CreateShapeDTO) {
-  //   return this.shapeService.create(body);
-  // }
+  @Post()
+  create(@Body() body: CreateShapeDTO) {
+    return this.shapeService.create(body);
+  }
+
+  @Get(':shapeId')
+  findOne(@Param('shapeId') id: string) {
+    return this.shapeService.findOneID(+id);
+  }
 }
