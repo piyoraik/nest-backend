c82a1373fe3b74fe2f41a2c2ffc254783dee2f6d
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-22 11:51:24 +0900

    Color登録・参照できた

--- src/color/color.module.ts
+++ src/color/color.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarBodyNumberModule } from 'src/car-body-number/car-body-number.module';
 import { ColorRepository } from './color.repository';
 import { ColorService } from './color.service';
+import { ColorController } from './color.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ColorRepository])],
   providers: [ColorService],
   exports: [ColorService],
+  controllers: [ColorController],
 })
 export class ColorModule {}
