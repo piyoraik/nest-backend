a03434e296ab80d88085d15cf3f555a500beceaa
ryusei-ota <ota.ryusei@gmail.com>
2022-01-24 02:45:42 +0900

    DTO完

--- src/purchase-management/dto/create.purchase-management.dto.ts
+++ src/purchase-management/dto/create.purchase-management.dto.ts
@@ -3,87 +3,134 @@ import { IsDateString, IsNumber, IsString } from 'class-validator';
 
 export class CreatePurchaseManagementDTO {
   //仕入れ日
-  @ApiProperty()
+  @ApiProperty({
+    description: '仕入れ日',
+  })
   @IsDateString()
   purchaseData: Date;
 
   //仕入れ先
-  @ApiProperty()
+  @ApiProperty({
+    description: '仕入れ先',
+    default: 'HAL自動車代理店',
+  })
   @IsString()
   supplier: string;
 
   //仕入金額
-  @ApiProperty()
+  @ApiProperty({
+    description: '仕入金額',
+    default: 300000,
+  })
   @IsNumber()
   purchaseAmount: number;
 
-  //
-  @ApiProperty()
+  //経費
+  @ApiProperty({
+    description: '経費',
+    default: 30000,
+  })
   @IsNumber()
   expense: number;
 
   //名変期限
-  @ApiProperty()
+  @ApiProperty({
+    description: '名変期限',
+  })
   @IsDateString()
   nameStrangePeriod: Date;
 
   //備考
-  @ApiProperty()
+  @ApiProperty({
+    description: '備考',
+    default: '特になし',
+  })
   @IsString()
   note: string;
 
   //仕入原価
-  @ApiProperty()
+  @ApiProperty({
+    description: '仕入原価',
+    default: 250000,
+  })
   @IsNumber()
   purchasingCost: number;
 
   //消費税
-  @ApiProperty()
+  @ApiProperty({
+    description: '消費税',
+    default: 25000,
+  })
   @IsNumber()
   tax: number;
 
   //売上日
-  @ApiProperty()
+  @ApiProperty({
+    description: '売上日',
+  })
   @IsDateString()
   salesDate: Date;
 
   //売上先
-  @ApiProperty()
+  @ApiProperty({
+    description: '売上先',
+    default: 'モードモータース',
+  })
   @IsString()
   salesTarget: string;
 
   //担当
-  @ApiProperty()
+  @ApiProperty({
+    description: '担当',
+    default: '河田',
+  })
   @IsString()
   rep: string;
 
   //売上
-  @ApiProperty()
+  @ApiProperty({
+    description: '売上',
+    default: 120000,
+  })
   @IsNumber()
   sales: number;
 
   //手数料
-  @ApiProperty()
+  @ApiProperty({
+    description: '手数料',
+    default: 20000,
+  })
   @IsNumber()
   fee: number;
 
   //売上合計
-  @ApiProperty()
+  @ApiProperty({
+    description: '売上合計',
+    default: 400000,
+  })
   @IsNumber()
   salesTotal: number;
 
   //純利益
-  @ApiProperty()
+  @ApiProperty({
+    description: '純利益',
+    default: 50000,
+  })
   @IsNumber()
   netProfit: number;
 
   //自税預
-  @ApiProperty()
+  @ApiProperty({
+    description: '自税預',
+    default: 10000,
+  })
   @IsNumber()
   selfTax: number;
 
   //名変更日
-  @ApiProperty()
+  @ApiProperty({
+    description: '名変更日',
+  })
   @IsDateString()
   nameChangeDate: Date;
 }
